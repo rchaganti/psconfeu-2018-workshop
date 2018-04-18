b81404a74ed8266761f771b5dc76889bb0258b1c
rchaganti <ravikanth@ravichaganti.com>
2018-04-18 19:06:02 +0200

    Merge branch 'master' of https://github.com/rchaganti/psconfeu-2018-workshop

--- Greet.psm1
+++ Greet.psm1
@@ -3,48 +3,10 @@ function New-Greeting() {
     param(
         [Parameter(Mandatory=$true, 
             ValueFromPipeline=$true)]
-        [string]
-        $Name,
-
-        [Parameter(ValueFromPipeline=$true)]
-        [string]
-        $Greeting = 'Hello'
+        [string]$Name
     )
 
     process {
-        $Greeting + " " + $Name
-    }
-}
-
-function Get-Name {
-    [CmdletBinding()]
-    [OutputType([string[]])]
-    param
-    (
-        [Parameter(Mandatory = $true, ParameterSetName = 'DefaultGreeting')]
-        [Parameter(Mandatory = $true, ParameterSetName = 'CustomGreeting')]
-        [String[]]
-        $Name,
-
-        [Parameter(ParameterSetName = 'DefaultGreeting')]
-        [Switch]
-        $UseDefaultGreeting,
-
-        [Parameter(ParameterSetName = 'CustomGreeting')]
-        [String]
-        $Greeting
-    )
-
-    Process
-    {
-        if ($PSBoundParameters.ContainsKey('UseDefaultGreeting'))
-        {
-            $nameObject = @{}
-            #DO Something
-        }
-        else
-        {
-            #Do something else    
-        }
+        "Hello " + $Name
     }
 }
